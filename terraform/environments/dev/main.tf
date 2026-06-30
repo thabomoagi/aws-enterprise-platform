2cd7714939331beed6b7ad7d850ad7d6bf60cd5b
Thabo Moagi <tboydaflame@gmail.com>
2026-06-30 15:38:32 +0200

    fix: format code and exclude local terraform binaries

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 module "development_network" {
   source = "../../modules/vpc"
 
-  environment          = "dev"
-  vpc_cidr             = "10.0.0.0/16"
-  public_subnet_cidr   = "10.0.1.0/24"
-  private_subnet_cidr  = "10.0.2.0/24"
+  environment         = "dev"
+  vpc_cidr            = "10.0.0.0/16"
+  public_subnet_cidr  = "10.0.1.0/24"
+  private_subnet_cidr = "10.0.2.0/24"
 }
